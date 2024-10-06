8c1ed7b54c284ed891004c9f4d72f076533fdb72
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-10-06 22:42:22 +0530

    .

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+// components
+
+import Login from './components/account/Login';
+
+function App() {
+  return (
+    <div className="App" style={{ marginTop: 64}}>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/account/Login.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import React from 'react';
+import { Box, TextField, Button, styled, Typography } from '@mui/material';
+
+const Component = styled(Box)`
+    width: 400px;
+    margin: auto;
+    box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+`
+const Image = styled('img')({
+    width: 200,
+    margin: 'auto',
+    display: 'flex',
+    padding: '50px 0 0'
+})
+    
+
+const Wrapper = styled(Box)`
+    padding: 25px 35px;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    & > div, & > button, & > p {
+        margin-top: 30px;
+    }
+
+`
+
+const LoginButton = styled(Button)`
+    text-transform: none;
+    background: #1D0FE3FF;
+    color: white;
+    height: 50px;
+    border-radius: 2px;
+`
+const SignupButton = styled(Button)`
+    text-transform: none;
+    background: white;
+    color: blue;
+    height: 50px;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
+`;
+
+const Text = styled(Typography)`
+    color: gray;
+    font-size: 16px;
+`
+const signupInValue = {
+    name: '',
+    username: '',
+    password: ''
+}
+
+const Login = () => {
+
+    const [account, toggleAccount ] = useState('login');
+    const [signup, setSignup] = useState(signupInValue);
+
+    const imageUrl = 'https://th.bing.com/th/id/OIP.wbHi1nzv-pNypLiwryUt7QHaFj?w=240&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7';
+
+    const toggleSignup = () => {
+        account === 'signup' ? toggleAccount('login') : toggleAccount('signup');
+    }
+
+    const onInputChange = (e) => {
+        setSignup({ ...signup, [e.target.name] : e.target.value })
+    }
+
+  return (
+    <div>
+      <Component>
+        <Box>
+            <Image src={ imageUrl } alt="login" />
+            {
+                account === 'login' ?
+                    <Wrapper>
+                        <label>Login</label>
+                        <TextField placeholder='Username' variant='standard'/>
+                        <TextField placeholder='Password' variant='standard'/>
+                        <LoginButton variant="contained">Login</LoginButton>
+                        <Text>OR</Text>
+
+                        <SignupButton onClick={() => toggleSignup()}>Create an account</SignupButton>
+                    </Wrapper> 
+                :
+                    <Wrapper>
+                        <label>Signup</label>
+                        <TextField name='name' placeholder='Name' onChange={(e) => onInputChange()} variant='standard'/>
+                        <TextField name='username' placeholder='Username' onChange={(e) => onInputChange()} variant='standard'/>
+                        <TextField name='password' placeholder='Password' onChange={(e) => onInputChange()} variant='standard'/>
+                        <SignupButton variant="contained">Signup</SignupButton>
+                        <Text>OR</Text>
+
+                        <LoginButton onClick={() => toggleSignup()}>Already have account</LoginButton>
+                    </Wrapper>
+            }
+
+         </Box>
+      </Component>
+    </div>
+  )
+}
+
+export default Login
