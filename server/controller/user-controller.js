95cae2d84684753934da8b4998b2089000a6edee
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-10-07 15:18:25 +0530

    Update user-controller.js

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,11 +1,20 @@
 
-
+import bcrypt from 'bcrypt';
 import User from '../model/user.js';
+import { request } from 'express';
+// import { name } from 'body-parser';
 
 export const signupUser = async (req, res) => {
     try {
-        const user = req.body; // Retrieve user data from request body
-        console.log('User data received:', user); // Log received user data
+        // const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(request.body.password, 10);
+
+        const user = { username: request.body.username, name: request.body.name,password: hashedPassword };
+
+
+
+        const User = req.body; // Retrieve user data from request body
+        console.log('User data received:', Userser); // Log received user data
 
         const newUser = new User(user);
         await newUser.save(); // Save the user in the database
