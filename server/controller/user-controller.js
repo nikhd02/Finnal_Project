7dd7b4191af7678e20bad0b96e1dd03bb449c1c6
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-10-07 14:45:34 +0530

    Update user-controller.js

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,21 +1,5 @@
 
 
-
-// import User from '../model/user.js'
-
-// export const signupUser = async (req, res) => {
-//     try{
-//         const user = req.body;
-
-//         const newUser = new User(user);
-//         await newUser.save();
-
-//         return res.status(200).json({ msg: 'Signup successful'})
-//     } catch(error){
-//         return res.status(500).json({msg: 'Error while signup the user'})
-//     }
-// }
-
 import User from '../model/user.js';
 
 export const signupUser = async (req, res) => {
